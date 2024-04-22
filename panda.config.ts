a35b1c67a60dc77263255ce1017570bc57f47fb9
kylie <kylie@penxle.io>
2024-04-23 01:05:38 +0900

    remove panda config hash option

--- panda.config.ts
+++ panda.config.ts
@@ -3,13 +3,10 @@ import { defineConfig } from '@pandacss/dev'
 import { globalCss } from './src/styles/global'
 import { tokens } from './src/styles/tokens'
 
-const prod = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
   preflight: true,
   include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
   exclude: [],
-  hash: prod,
 
   theme: {
     extend: {
