2ccb7fe49cea7d18bedc240fd08d54ade0deb56b
kylie <kylie@penxle.io>
2024-04-25 03:22:35 +0900

    add map marker image

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,4 +1,4 @@
-import { CustomOverlayMap, Map, useKakaoLoader } from 'react-kakao-maps-sdk'
+import { Map, MapMarker, useKakaoLoader } from 'react-kakao-maps-sdk'
 import { css } from '../../styled-system/css'
 
 function Maps() {
@@ -9,19 +9,28 @@ function Maps() {
   return (
     <>
       {loading ? (
-        <div className={css({ width: 'full', height: '360px' })}>지도 불러오는 중</div>
+        <div className={css({ width: 'full', height: '360px' })}>지도 불러오는 중..</div>
       ) : (
         <Map center={{ lat: 36.356586, lng: 127.351549 }} style={{ width: '100%', height: '360px' }} level={2}>
-          <CustomOverlayMap position={{ lat: 36.356586, lng: 127.351549 }}>
-            <a
-              className={css({ backgroundColor: 'gray.400' })}
-              href="https://kko.to/CL7UohSdt1"
-              target="_blank"
-              rel="noreferrer"
-            >
-              대전 유성컨벤션
-            </a>
-          </CustomOverlayMap>
+          <MapMarker
+            position={{
+              lat: 36.356586,
+              lng: 127.351549,
+            }}
+            image={{
+              src: 'https://tmpfiles.nohat.cc/full-m2H7K9i8Z5K9Z5d3.png',
+              size: {
+                width: 26,
+                height: 32,
+              },
+              options: {
+                offset: {
+                  x: 13,
+                  y: 38,
+                },
+              },
+            }}
+          />
         </Map>
       )}
     </>
