d544848b10ee61440a25ad0b4d93351109e9891d
kylie <kylie@penxle.io>
2024-05-05 01:22:22 +0900

    update map pin image

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,6 +1,8 @@
 import { Map, MapMarker, useKakaoLoader } from 'react-kakao-maps-sdk'
 import { css } from '../../styled-system/css'
 
+import MapPin from '../assets/map-pin.png'
+
 function Maps() {
   const [loading] = useKakaoLoader({
     appkey: import.meta.env.VITE_API_KAKAO_KEY,
@@ -9,16 +11,16 @@ function Maps() {
   return (
     <>
       {loading ? (
-        <div className={css({ width: 'full', height: '360px' })}>지도 불러오는 중..</div>
+        <div className={css({ width: 'full', height: '340px' })}>지도 불러오는 중..</div>
       ) : (
-        <Map center={{ lat: 36.356586, lng: 127.351549 }} style={{ width: '100%', height: '360px' }} level={2}>
+        <Map center={{ lat: 36.356586, lng: 127.351549 }} style={{ width: '100%', height: '340px' }} level={2}>
           <MapMarker
             position={{
               lat: 36.356586,
               lng: 127.351549,
             }}
             image={{
-              src: 'https://tmpfiles.nohat.cc/full-m2H7K9i8Z5K9Z5d3.png',
+              src: MapPin,
               size: {
                 width: 26,
                 height: 32,
